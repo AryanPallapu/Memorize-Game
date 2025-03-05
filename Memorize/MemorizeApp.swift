d6597ff21a399f5f091bc202cc7c10774717755f
Aryan Pallapu <aryan.pallapu@gmail.com>
2025-03-06 05:29:23 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by sri veda aryan on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
