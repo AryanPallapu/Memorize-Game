97029b370d78018160e77787ca9dbd530be52911
Sri Veda Aryan Pallapu <113664664+AryanPallapu@users.noreply.github.com>
2025-03-06 06:33:33 +0530

    Update MemorizeApp.swift

--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -4,14 +4,15 @@
 //
 //  Created by sri veda aryan on 06/03/25.
 //
-
 import SwiftUI
 
 @main
 struct MemorizeApp: App {
+    @StateObject var game = EmojiMemoryGame()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiMemoryGameView(viewModel: game)
         }
     }
 }
